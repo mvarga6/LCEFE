6e76d4d3b167b44a4df58049e4bfb10642ab409d
akonya <akonya1@gmail.com>
2013-10-08 16:04:55 -0400

    better beam motion

--- packdata.h
+++ packdata.h
@@ -52,8 +52,8 @@ void packdata(NodeArray &i_Node,TetArray &i_Tet, HostDataBlock *dat,int Ntets,in
 		}//sweep
 
     //add force to end of beam
-    if(i_Node.get_pos(nod,0)>39.0){
-      dat->host_v[nod+Nnodes*1] = 10.0;
+    if(i_Node.get_pos(nod,0)>39.9){
+      dat->host_v[nod+Nnodes*2] =100.0;
     }//if rx >39.0
 
 		for(int rank=0;rank<MaxNodeRank;rank++){
