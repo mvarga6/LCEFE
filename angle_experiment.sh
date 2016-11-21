7b81df955d81d3dd59d9ebe5cd0edcd80bf176ac
mvarga6 <mvarga6@kent.edu>
2016-11-21 18:45:42 -0500

    Change datadir in angle_experiment.sh

--- angle_experiment.sh
+++ angle_experiment.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-DATADIR="/nfs/02/ksu0236/mvarga/VTK"
+DATADIR="/nfs/02/ksu0236/mvarga/LCEOUT"
 [ -d $DATADIR ] || mkdir $DATADIR
 cd $DATADIR
 
 for p in 90 85 80 75 70 65 60 55 50
 do
-	qsub -v phi=${p},smax=0,smin=-0.75,sqh=0.18,sql=0.9,out=A_phi${p} ~/mvarga/LCEFE/myscript.job
+	qsub -v phi=${p},smax=0,smin=-1.0,sqh=0.18,sql=0.9,out=A_phi${p} ~/mvarga/LCEFE/myscript.job
 done
