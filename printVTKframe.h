7723524311e0ca4cee0ae6e9e57af0c1b7b96507
mvarga6 <mvarga6@kent.edu>
2016-11-22 14:00:46 -0500

    Change resolution of light grid.

--- printVTKframe.h
+++ printVTKframe.h
@@ -59,7 +59,7 @@ void printVTKframe(   DevDataBlock *dev_dat
 				Ntets, 
 				Nnodes, 
 				host_dat->host_S, 
-				0.5*meshScale, 0.5*meshScale);
+				0.15*meshScale, 0.15*meshScale);
 	}
 
 	//.. copy new S to device
