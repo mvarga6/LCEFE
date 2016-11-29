38036061fd70694f0ade07a28fa16ed60fc2069b
mvarga6 <mvarga6@kent.edu>
2016-11-29 18:21:50 -0500

    Add flags option to myscript.job for arbitrary options to sim exe.

--- angle_experiment.sh
+++ angle_experiment.sh
@@ -6,5 +6,6 @@ cd $DATADIR
 for p in 90 85 80 75 70 65 60 55 50
 do
 	qsub -v datadir=$DATADIR,phi=${p},smax=0,smin=-1.0,sqh=0.15,sql=0.95,out=planartop_phi${p} ~/mvarga/LCEFE/myscript.job
-	qsub -v datadir=$DATADIR,phi=${p},smax=0,smin=-1.0,sqh=0.13,sql=0.975,out=homeotop_phi${p} ~/mvarga/LCEFE/myscript2.job
+	qsub -v datadir=$DATADIR,flags=--homeotop,phi=${p},smax=0,smin=-1.0,sqh=0.15,sql=0.95,out=homeotop_phi${p} ~/mvarga/LCEFE/myscript.job
+#	qsub -v datadir=$DATADIR,phi=${p},smax=0,smin=-1.0,sqh=0.13,sql=0.975,out=homeotop_phi${p} ~/mvarga/LCEFE/myscript2.job
 done
