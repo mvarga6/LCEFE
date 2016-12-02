9ee33fba9c4b44be11ed3d3d8156a5f773db0ad2
mvarga6 <mvarga6@kent.edu>
2016-12-01 23:46:33 -0500

    Change angle experiment.

--- angle_experiment.sh
+++ angle_experiment.sh
@@ -5,7 +5,7 @@ cd $DATADIR
 
 for p in 90 85 80 75 70 65 60 55 50
 do
-	qsub -v datadir=$DATADIR,phi=${p},smax=0,smin=-1.0,sqh=0.15,sql=0.94,out=planartop_phi${p} ~/mvarga/LCEFE/myscript.job
+	qsub -v datadir=$DATADIR,phi=${p},smax=0,smin=-1.0,sqh=0.1,sql=0.94,out=planartop_phi${p} ~/mvarga/LCEFE/myscript.job
 	qsub -v datadir=$DATADIR,flags=--homeotop,phi=${p},smax=0,smin=-1.0,sqh=0.075,sql=0.99,out=homeotop_phi${p} ~/mvarga/LCEFE/myscript.job
 #	qsub -v datadir=$DATADIR,phi=${p},smax=0,smin=-1.0,sqh=0.13,sql=0.975,out=homeotop_phi${p} ~/mvarga/LCEFE/myscript2.job
 done
