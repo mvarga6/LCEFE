91b7ac1431dbc7a090240fbfbb0121b743713da3
mvarga6 <mvarga6@kent.edu>
2016-12-07 19:16:28 -0500

    Zdim deform to 0.75, drag to 0.9999

--- angle_experiment.sh
+++ angle_experiment.sh
@@ -5,6 +5,6 @@ cd $DATADIR
 
 for p in 90 85 80 75 70 65 60 55 50
 do
-	qsub -v datadir=$DATADIR,phi=${p},smax=0,smin=-1.0,sqh=0.08,sql=0.96,out=planartop_phi${p} ~/mvarga/LCEFE/myscript.job
-	qsub -v datadir=$DATADIR,flags=--homeotop,phi=${p},smax=0,smin=-1.0,sqh=0.08,sql=0.96,out=homeotop_phi${p} ~/mvarga/LCEFE/myscript.job
+	qsub -v datadir=$DATADIR,phi=${p},smax=0,smin=-1.0,sqh=0.1,sql=0.925,out=planartop_phi${p} ~/mvarga/LCEFE/myscript.job
+	qsub -v datadir=$DATADIR,flags=--homeotop,phi=${p},smax=0,smin=-1.0,sqh=0.05,sql=0.99,out=homeotop_phi${p} ~/mvarga/LCEFE/myscript.job
 done
