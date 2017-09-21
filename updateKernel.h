6cdec25cc3c173e80bbe48f914561118cf3512bb
Mike Varga <mvarga6@kent.edu>
2017-09-21 16:36:48 -0400

    Continue to cleanup.  Deal with all warnings.

--- updateKernel.h
+++ updateKernel.h
@@ -9,24 +9,25 @@
 // -use velocity verilet to step system forward
 // -send updated positions to global memory 
 //================================================
-__global__ void updateKernel(	 float *dF
-				,int pitchdF
-				,float *F
-				,int pitchF
-				,int Nnodes 
-				,int *NodeRank
-				,float *v
-				,int pitchv
-				,float *r
-				,int pitchr	
-				,float *mass
-				,float xclamp1, float xclamp2
-				,float ztable){  // puts sim on table
-	
-	int dFshift = pitchdF/sizeof(float);
-	int Fshift = pitchF/sizeof(float);
-	int vshift = pitchv/sizeof(float);
-	int rshift = pitchr/sizeof(float);
+/*__global__ void updateKernel(	 float *dF*/
+/*				,int pitchdF*/
+/*				,float *F*/
+/*				,int pitchF*/
+/*				,int Nnodes */
+/*				,int *NodeRank*/
+/*				,float *v*/
+/*				,int pitchv*/
+/*				,float *r*/
+/*				,int pitchr	*/
+/*				,float *mass*/
+/*				,float xclamp1, float xclamp2*/
+/*				,float ztable){  // puts sim on table*/
+__global__ void updateKernel(DevDataBlock data, float xclamp1, float xclamp2, float ztable)
+{	
+	int dFshift = data.dFpitch/sizeof(float);
+	int Fshift = data.Fpitch/sizeof(float);
+	int vshift = data.vpitch/sizeof(float);
+	int rshift = data.rpitch/sizeof(float);
 	int myNode;
 	int myNodeRank;
 	float Fnew[3]={0.0};
@@ -37,20 +38,21 @@ __global__ void updateKernel(	 float *dF
 	//thread ID
 	int tid = threadIdx.x + blockIdx.x * blockDim.x;
 
-	if (tid<Nnodes){  //if a node is here
-		myNode=tid;
-		myNodeRank = NodeRank[myNode];
-		localMass = mass[myNode];
+	if (tid < data.Nnodes)
+	{  //if a node is here
+		myNode = tid;
+		myNodeRank = data.nodeRank[myNode];
+		localMass = data.m[myNode];
 
 		//get new and old forces + old velocities
-		sumForce(myNode,myNodeRank,Fnew,Fold,vold,dF,dFshift,F,Fshift,v,vshift);
+		sumForce(myNode, myNodeRank, Fnew, Fold, vold, data.dF, dFshift, data.F, Fshift, data.v, vshift);
 
 		//calculate and store new velocites
-		update_v(vnew,vold,Fold,Fnew,v,vshift,myNode,localMass);
+		update_v(vnew, vold, Fold, Fnew, data.v, vshift, myNode, localMass);
 
 		//calculate and store new positions
 		float xclamps[2] = { xclamp1, xclamp2 };
-		update_r(r,rshift,vnew,Fnew,myNode,localMass,xclamps,ztable);
+		update_r(data.r, rshift, vnew, Fnew, myNode, localMass, xclamps, ztable);
 
 	}//tid<Nnodes
 }//updateKernel
