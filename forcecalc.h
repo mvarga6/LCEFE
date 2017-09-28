1e003d81bae83d4f599a4633032e4b171b677970
Mike Varga <mvarga6@kent.edu>
2017-09-28 19:11:25 -0400

    Move all .cpp files into src directory and build in two steps: build object files into obj directory, then link to build gafe6 in builds directory.  All physics methods in force_calc are now static methods of Physics class.  Next, continue to seperate into multiple .cpp files.  Makefile handles all of build steps, no longer using seperate build script.

--- forcecalc.h
+++ forcecalc.h
@@ -5,146 +5,154 @@
 #include "parameters.h"
 #include "UserDefined.h"
 #include "constant_cuda_defs.h"
+#include "physics_model.h"
 
 //=============================================================
 //calculate forces on all 4 nodes in tetrahedra
 //=============================================================
-__device__ void force_calc(float *Ainv,float *r0,float *r,float *Q,float (&F)[12],int *TetNodeRank,float *pe,int mytet,float myVol){
+__device__ 
+void force_calc(float *Ainv,float *r0,float *r,float *Q,float (&F)[12],int *TetNodeRank,float *pe,int mytet,float myVol){
 
-	float u[4],v[4],w[4];
+	//float u[4],v[4],w[4];
 	float eps[9];
-	float a[4] = {0.0};
-	float b[4] = {0.0};
-	float c[4] = {0.0};
+	float a[4], b[4], c[4];
 	float localPe = 0.0;
+	float lcEnergy = 0.0f;
 	const float cxxxx = Parameters.Cxxxx;
 	const float cxxyy = Parameters.Cxxyy;
 	const float cxyxy = Parameters.Cxyxy;
 	const float alpha = Parameters.Alpha;
 
-	//clacluate displacements from original position and zero out forces
-	for(int n=0;n<4;n++){
-		u[n] = (r[n*3]-r0[n*3]);
-		v[n] = (r[1+n*3]-r0[1+n*3]);
-		w[n] = (r[2+n*3]-r0[2+n*3]);
-		F[0+n*3] = 0.0;
-		F[1+n*3] = 0.0;
-		F[2+n*3] = 0.0;
-	}//n
+	Physics::CalculateShapeFunction(a, b, c, r, r0, Ainv);
+	Physics::CalculateEpsilon(eps, a, b, c);
+	//Physics::CalculateElasticPotential(localPe, eps, cxxxx, cxxyy, cxyxy);
+	Physics::CalculateLiquidCrystalEnergy(lcEnergy, eps, Q, alpha);
+	Physics::AddElasticForces(F, eps, Ainv, a, b, c, cxxxx, cxxyy, cxyxy);
+	Physics::AddLiquidCrystalForces(F, Q, Ainv, a, b, c, alpha);
+	pe[mytet] = (localPe + lcEnergy) * myVol;
 
-	//matrix multipy Ainv and u,v,w to get shape funcitons
-	for(int i=0;i<4;i++){
-		for(int j=0;j<4;j++){
-			a[i]+=Ainv[4*i+j]*u[j];
-			b[i]+=Ainv[4*i+j]*v[j];
-			c[i]+=Ainv[4*i+j]*w[j];
-		}//j
-	}//i
+/*//clacluate displacements from original position and zero out forces*/
+/*	for(int n=0;n<4;n++){*/
+/*		u[n] = (r[n*3]-r0[n*3]);*/
+/*		v[n] = (r[1+n*3]-r0[1+n*3]);*/
+/*		w[n] = (r[2+n*3]-r0[2+n*3]);*/
+/*		F[0+n*3] = 0.0;*/
+/*		F[1+n*3] = 0.0;*/
+/*		F[2+n*3] = 0.0;*/
+/*	}//n*/
+
+/*	//matrix multipy Ainv and u,v,w to get shape funcitons*/
+/*	for(int i=0;i<4;i++){*/
+/*		for(int j=0;j<4;j++){*/
+/*			a[i]+=Ainv[4*i+j]*u[j];*/
+/*			b[i]+=Ainv[4*i+j]*v[j];*/
+/*			c[i]+=Ainv[4*i+j]*w[j];*/
+/*		}//j*/
+/*	}//i*/
 
 	//noe we calculate epsilon tensor
-	eps[3*0+0] = a[1]+0.5*(a[1]*a[1]+b[1]*b[1]+c[1]*c[1]);
-	eps[3*1+1] = b[2]+0.5*(a[2]*a[2]+b[2]*b[2]+c[2]*c[2]);
-	eps[3*2+2] = c[3]+0.5*(a[3]*a[3]+b[3]*b[3]+c[3]*c[3]);
-	eps[3*0+1] = 0.5*(a[2]+b[1]+a[1]*a[2]+b[1]*b[2]+c[1]*c[2]);
-	eps[3*1+0] = eps[3*0+1];
-	eps[3*0+2] = 0.5*(a[3]+c[1]+a[1]*a[3]+b[1]*b[3]+c[1]*c[3]);
-	eps[3*2+0] = eps[3*0+2];
-	eps[3*1+2] = 0.5*(b[3]+c[2]+a[2]*a[3]+b[2]*b[3]+c[2]*c[3]);
-	eps[3*2+1] = eps[3*1+2];
+/*	eps[3*0+0] = a[1]+0.5*(a[1]*a[1]+b[1]*b[1]+c[1]*c[1]);*/
+/*	eps[3*1+1] = b[2]+0.5*(a[2]*a[2]+b[2]*b[2]+c[2]*c[2]);*/
+/*	eps[3*2+2] = c[3]+0.5*(a[3]*a[3]+b[3]*b[3]+c[3]*c[3]);*/
+/*	eps[3*0+1] = 0.5*(a[2]+b[1]+a[1]*a[2]+b[1]*b[2]+c[1]*c[2]);*/
+/*	eps[3*1+0] = eps[3*0+1];*/
+/*	eps[3*0+2] = 0.5*(a[3]+c[1]+a[1]*a[3]+b[1]*b[3]+c[1]*c[3]);*/
+/*	eps[3*2+0] = eps[3*0+2];*/
+/*	eps[3*1+2] = 0.5*(b[3]+c[2]+a[2]*a[3]+b[2]*b[3]+c[2]*c[3]);*/
+/*	eps[3*2+1] = eps[3*1+2];*/
 
 	//calculate potential energy
-	localPe += cxxxx*(eps[3*0+0]*eps[3*0+0]+eps[3*1+1]*eps[3*1+1]+eps[3*2+2]*eps[3*2+2]);
-	localPe += 2.0*cxxyy*(eps[3*0+0]*eps[3*1+1]+eps[3*1+1]*eps[3*2+2]+eps[3*0+0]*eps[3*2+2]);
-	localPe += 4.0*cxyxy*(eps[3*0+1]*eps[3*0+1]+eps[3*1+2]*eps[3*1+2]+eps[3*2+0]*eps[3*2+0]);
+/*	localPe += cxxxx*(eps[3*0+0]*eps[3*0+0]+eps[3*1+1]*eps[3*1+1]+eps[3*2+2]*eps[3*2+2]);*/
+/*	localPe += 2.0*cxxyy*(eps[3*0+0]*eps[3*1+1]+eps[3*1+1]*eps[3*2+2]+eps[3*0+0]*eps[3*2+2]);*/
+/*	localPe += 4.0*cxyxy*(eps[3*0+1]*eps[3*0+1]+eps[3*1+2]*eps[3*1+2]+eps[3*2+0]*eps[3*2+0]);*/
 
-	localPe += -1.0*alpha*(eps[3*0+0]*Q[3*0+0]
-				+eps[3*1+1]*Q[3*1+1]+eps[3*2+2]*Q[3*2+2]
-				+eps[3*0+1]*Q[3*0+1]+eps[3*1+0]*Q[3*1+0]
-				+eps[3*1+2]*Q[3*1+2]+eps[3*2+1]*Q[3*2+1]
-				+eps[3*0+2]*Q[3*0+2]+eps[3*2+0]*Q[3*2+0]);
-
+/*	localPe += -1.0*alpha*(eps[3*0+0]*Q[3*0+0]*/
+/*				+eps[3*1+1]*Q[3*1+1]+eps[3*2+2]*Q[3*2+2]*/
+/*				+eps[3*0+1]*Q[3*0+1]+eps[3*1+0]*Q[3*1+0]*/
+/*				+eps[3*1+2]*Q[3*1+2]+eps[3*2+1]*Q[3*2+1]*/
+/*				+eps[3*0+2]*Q[3*0+2]+eps[3*2+0]*Q[3*2+0]);*/
 
 	//send potential to global memory
-	pe[mytet] = localPe*myVol;
+/*	pe[mytet] = localPe * myVol;*/
 
-	//now can calculate forces
-	for(int n = 0;n<4;n++){
-		//x
-		
-		F[0+n*3]+=-cxxxx*2.0*eps[3*0+0]*Ainv[4*1+n]*(1.0+a[1]);
-		F[0+n*3]+=-cxxxx*2.0*eps[3*1+1]*Ainv[4*2+n]*a[2];
-		F[0+n*3]+=-cxxxx*2.0*eps[3*2+2]*Ainv[4*3+n]*a[3];
-		F[0+n*3]+=-2.0*cxxyy*eps[3*0+0]*Ainv[4*2+n]*a[2];
-		F[0+n*3]+=-2.0*cxxyy*eps[3*1+1]*Ainv[4*1+n]*(1.0+a[1]);
-		F[0+n*3]+=-2.0*cxxyy*eps[3*1+1]*Ainv[4*3+n]*a[3];
-		F[0+n*3]+=-2.0*cxxyy*eps[3*2+2]*Ainv[4*2+n]*a[2];
-		F[0+n*3]+=-2.0*cxxyy*eps[3*2+2]*Ainv[4*1+n]*(1.0+a[1]);
-		F[0+n*3]+=-2.0*cxxyy*eps[3*0+0]*Ainv[4*3+n]*a[3];
-		F[0+n*3]+=-4.0*cxyxy*eps[3*0+1]*Ainv[4*2+n]*(1.0+a[1]);
-		F[0+n*3]+=-4.0*cxyxy*eps[3*0+1]*Ainv[4*1+n]*a[2];
-		F[0+n*3]+=-4.0*cxyxy*eps[3*1+2]*Ainv[4*3+n]*a[2];
-		F[0+n*3]+=-4.0*cxyxy*eps[3*1+2]*Ainv[4*2+n]*a[3];
-		F[0+n*3]+=-4.0*cxyxy*eps[3*2+0]*Ainv[4*3+n]*(1.0+a[1]);
-		F[0+n*3]+=-4.0*cxyxy*eps[3*2+0]*Ainv[4*1+n]*a[3];
+/*	//now can calculate forces*/
+/*	for(int n = 0;n<4;n++){*/
+/*		//x*/
+/*		*/
+/*		F[0+n*3]+=-cxxxx*2.0*eps[3*0+0]*Ainv[4*1+n]*(1.0+a[1]);*/
+/*		F[0+n*3]+=-cxxxx*2.0*eps[3*1+1]*Ainv[4*2+n]*a[2];*/
+/*		F[0+n*3]+=-cxxxx*2.0*eps[3*2+2]*Ainv[4*3+n]*a[3];*/
+/*		F[0+n*3]+=-2.0*cxxyy*eps[3*0+0]*Ainv[4*2+n]*a[2];*/
+/*		F[0+n*3]+=-2.0*cxxyy*eps[3*1+1]*Ainv[4*1+n]*(1.0+a[1]);*/
+/*		F[0+n*3]+=-2.0*cxxyy*eps[3*1+1]*Ainv[4*3+n]*a[3];*/
+/*		F[0+n*3]+=-2.0*cxxyy*eps[3*2+2]*Ainv[4*2+n]*a[2];*/
+/*		F[0+n*3]+=-2.0*cxxyy*eps[3*2+2]*Ainv[4*1+n]*(1.0+a[1]);*/
+/*		F[0+n*3]+=-2.0*cxxyy*eps[3*0+0]*Ainv[4*3+n]*a[3];*/
+/*		F[0+n*3]+=-4.0*cxyxy*eps[3*0+1]*Ainv[4*2+n]*(1.0+a[1]);*/
+/*		F[0+n*3]+=-4.0*cxyxy*eps[3*0+1]*Ainv[4*1+n]*a[2];*/
+/*		F[0+n*3]+=-4.0*cxyxy*eps[3*1+2]*Ainv[4*3+n]*a[2];*/
+/*		F[0+n*3]+=-4.0*cxyxy*eps[3*1+2]*Ainv[4*2+n]*a[3];*/
+/*		F[0+n*3]+=-4.0*cxyxy*eps[3*2+0]*Ainv[4*3+n]*(1.0+a[1]);*/
+/*		F[0+n*3]+=-4.0*cxyxy*eps[3*2+0]*Ainv[4*1+n]*a[3];*/
 
-		//y
-		
-		F[1+n*3]+=-cxxxx*2.0*eps[3*0+0]*Ainv[4*1+n]*b[1];
-		F[1+n*3]+=-cxxxx*2.0*eps[3*1+1]*Ainv[4*2+n]*(1.0+b[2]);
-		F[1+n*3]+=-cxxxx*2.0*eps[3*2+2]*Ainv[4*3+n]*b[3];
-		F[1+n*3]+=-2.0*cxxyy*eps[3*0+0]*Ainv[4*2+n]*(1.0+b[2]);
-		F[1+n*3]+=-2.0*cxxyy*eps[3*1+1]*Ainv[4*1+n]*b[1];
-		F[1+n*3]+=-2.0*cxxyy*eps[3*1+1]*Ainv[4*3+n]*b[3];
-		F[1+n*3]+=-2.0*cxxyy*eps[3*2+2]*Ainv[4*2+n]*(1.0+b[2]);
-		F[1+n*3]+=-2.0*cxxyy*eps[3*2+2]*Ainv[4*1+n]*b[1];
-		F[1+n*3]+=-2.0*cxxyy*eps[3*0+0]*Ainv[4*3+n]*b[3];
-		F[1+n*3]+=-4.0*cxyxy*eps[3*0+1]*Ainv[4*1+n]*(1.0+b[2]);
-		F[1+n*3]+=-4.0*cxyxy*eps[3*0+1]*Ainv[4*2+n]*b[1];
-		F[1+n*3]+=-4.0*cxyxy*eps[3*1+2]*Ainv[4*3+n]*(1.0+b[2]);
-		F[1+n*3]+=-4.0*cxyxy*eps[3*1+2]*Ainv[4*2+n]*b[3];
-		F[1+n*3]+=-4.0*cxyxy*eps[3*2+0]*Ainv[4*3+n]*b[1];
-		F[1+n*3]+=-4.0*cxyxy*eps[3*2+0]*Ainv[4*1+n]*b[3];
+/*		//y*/
+/*		*/
+/*		F[1+n*3]+=-cxxxx*2.0*eps[3*0+0]*Ainv[4*1+n]*b[1];*/
+/*		F[1+n*3]+=-cxxxx*2.0*eps[3*1+1]*Ainv[4*2+n]*(1.0+b[2]);*/
+/*		F[1+n*3]+=-cxxxx*2.0*eps[3*2+2]*Ainv[4*3+n]*b[3];*/
+/*		F[1+n*3]+=-2.0*cxxyy*eps[3*0+0]*Ainv[4*2+n]*(1.0+b[2]);*/
+/*		F[1+n*3]+=-2.0*cxxyy*eps[3*1+1]*Ainv[4*1+n]*b[1];*/
+/*		F[1+n*3]+=-2.0*cxxyy*eps[3*1+1]*Ainv[4*3+n]*b[3];*/
+/*		F[1+n*3]+=-2.0*cxxyy*eps[3*2+2]*Ainv[4*2+n]*(1.0+b[2]);*/
+/*		F[1+n*3]+=-2.0*cxxyy*eps[3*2+2]*Ainv[4*1+n]*b[1];*/
+/*		F[1+n*3]+=-2.0*cxxyy*eps[3*0+0]*Ainv[4*3+n]*b[3];*/
+/*		F[1+n*3]+=-4.0*cxyxy*eps[3*0+1]*Ainv[4*1+n]*(1.0+b[2]);*/
+/*		F[1+n*3]+=-4.0*cxyxy*eps[3*0+1]*Ainv[4*2+n]*b[1];*/
+/*		F[1+n*3]+=-4.0*cxyxy*eps[3*1+2]*Ainv[4*3+n]*(1.0+b[2]);*/
+/*		F[1+n*3]+=-4.0*cxyxy*eps[3*1+2]*Ainv[4*2+n]*b[3];*/
+/*		F[1+n*3]+=-4.0*cxyxy*eps[3*2+0]*Ainv[4*3+n]*b[1];*/
+/*		F[1+n*3]+=-4.0*cxyxy*eps[3*2+0]*Ainv[4*1+n]*b[3];*/
 
-		//z
-		
-		F[2+n*3]+=-cxxxx*2.0*eps[3*0+0]*Ainv[4*1+n]*c[1];
-		F[2+n*3]+=-cxxxx*2.0*eps[3*1+1]*Ainv[4*2+n]*c[2];
-		F[2+n*3]+=-cxxxx*2.0*eps[3*2+2]*Ainv[4*3+n]*(1.0+c[3]);
-		F[2+n*3]+=-2.0*cxxyy*eps[3*0+0]*Ainv[4*2+n]*c[2];
-		F[2+n*3]+=-2.0*cxxyy*eps[3*1+1]*Ainv[4*1+n]*c[1];
-		F[2+n*3]+=-2.0*cxxyy*eps[3*1+1]*Ainv[4*3+n]*(1.0+c[3]);
-		F[2+n*3]+=-2.0*cxxyy*eps[3*2+2]*Ainv[4*2+n]*c[2];
-		F[2+n*3]+=-2.0*cxxyy*eps[3*2+2]*Ainv[4*1+n]*c[1];
-		F[2+n*3]+=-2.0*cxxyy*eps[3*0+0]*Ainv[4*3+n]*(1.0+c[3]);
-		F[2+n*3]+=-4.0*cxyxy*eps[3*0+1]*Ainv[4*2+n]*c[1];
-		F[2+n*3]+=-4.0*cxyxy*eps[3*0+1]*Ainv[4*1+n]*c[2];
-		F[2+n*3]+=-4.0*cxyxy*eps[3*1+2]*Ainv[4*2+n]*(1.0+c[3]);
-		F[2+n*3]+=-4.0*cxyxy*eps[3*1+2]*Ainv[4*3+n]*c[2];
-		F[2+n*3]+=-4.0*cxyxy*eps[3*2+0]*Ainv[4*1+n]*(1.0+c[3]);
-		F[2+n*3]+=-4.0*cxyxy*eps[3*2+0]*Ainv[4*3+n]*c[1];
+/*		//z*/
+/*		*/
+/*		F[2+n*3]+=-cxxxx*2.0*eps[3*0+0]*Ainv[4*1+n]*c[1];*/
+/*		F[2+n*3]+=-cxxxx*2.0*eps[3*1+1]*Ainv[4*2+n]*c[2];*/
+/*		F[2+n*3]+=-cxxxx*2.0*eps[3*2+2]*Ainv[4*3+n]*(1.0+c[3]);*/
+/*		F[2+n*3]+=-2.0*cxxyy*eps[3*0+0]*Ainv[4*2+n]*c[2];*/
+/*		F[2+n*3]+=-2.0*cxxyy*eps[3*1+1]*Ainv[4*1+n]*c[1];*/
+/*		F[2+n*3]+=-2.0*cxxyy*eps[3*1+1]*Ainv[4*3+n]*(1.0+c[3]);*/
+/*		F[2+n*3]+=-2.0*cxxyy*eps[3*2+2]*Ainv[4*2+n]*c[2];*/
+/*		F[2+n*3]+=-2.0*cxxyy*eps[3*2+2]*Ainv[4*1+n]*c[1];*/
+/*		F[2+n*3]+=-2.0*cxxyy*eps[3*0+0]*Ainv[4*3+n]*(1.0+c[3]);*/
+/*		F[2+n*3]+=-4.0*cxyxy*eps[3*0+1]*Ainv[4*2+n]*c[1];*/
+/*		F[2+n*3]+=-4.0*cxyxy*eps[3*0+1]*Ainv[4*1+n]*c[2];*/
+/*		F[2+n*3]+=-4.0*cxyxy*eps[3*1+2]*Ainv[4*2+n]*(1.0+c[3]);*/
+/*		F[2+n*3]+=-4.0*cxyxy*eps[3*1+2]*Ainv[4*3+n]*c[2];*/
+/*		F[2+n*3]+=-4.0*cxyxy*eps[3*2+0]*Ainv[4*1+n]*(1.0+c[3]);*/
+/*		F[2+n*3]+=-4.0*cxyxy*eps[3*2+0]*Ainv[4*3+n]*c[1];*/
 
-		//force from Q
-		F[0+n*3]+= alpha*(Q[0*3+0]*Ainv[4*1+n]*(1.0+a[1])
-				  +Q[1*3+1]*Ainv[4*2+n]*a[2]
-				  +Q[2*3+2]*Ainv[4*3+n]*a[3]
-				  +Q[0*3+1]*(Ainv[4*2+n]*(1.0+a[1])+Ainv[4*1+n]*a[2])
-				  +Q[0*3+2]*(Ainv[4*3+n]*(1.0+a[1])+Ainv[4*1+n]*a[3])
-				  +Q[1*3+2]*(Ainv[4*3+n]*a[2]+Ainv[4*2+n]*a[3]));
+/*		//force from Q*/
+/*		F[0+n*3]+= alpha*(Q[0*3+0]*Ainv[4*1+n]*(1.0+a[1])*/
+/*				  +Q[1*3+1]*Ainv[4*2+n]*a[2]*/
+/*				  +Q[2*3+2]*Ainv[4*3+n]*a[3]*/
+/*				  +Q[0*3+1]*(Ainv[4*2+n]*(1.0+a[1])+Ainv[4*1+n]*a[2])*/
+/*				  +Q[0*3+2]*(Ainv[4*3+n]*(1.0+a[1])+Ainv[4*1+n]*a[3])*/
+/*				  +Q[1*3+2]*(Ainv[4*3+n]*a[2]+Ainv[4*2+n]*a[3]));*/
 
-		F[1+n*3]+= alpha*(Q[0*3+0]*Ainv[4*1+n]*b[1]
-				  +Q[1*3+1]*Ainv[4*2+n]*(1.0+b[2])
-				  +Q[2*3+2]*Ainv[4*3+n]*b[3]
-				  +Q[0*3+1]*(Ainv[4*1+n]*(1.0+b[2])+Ainv[4*2+n]*b[1])
-				  +Q[0*3+2]*(Ainv[4*3+n]*b[1]+Ainv[4*1+n]*b[3])
-				  +Q[1*3+2]*(Ainv[4*3+n]*(1.0+b[2])+Ainv[4*2+n]*b[3]));
+/*		F[1+n*3]+= alpha*(Q[0*3+0]*Ainv[4*1+n]*b[1]*/
+/*				  +Q[1*3+1]*Ainv[4*2+n]*(1.0+b[2])*/
+/*				  +Q[2*3+2]*Ainv[4*3+n]*b[3]*/
+/*				  +Q[0*3+1]*(Ainv[4*1+n]*(1.0+b[2])+Ainv[4*2+n]*b[1])*/
+/*				  +Q[0*3+2]*(Ainv[4*3+n]*b[1]+Ainv[4*1+n]*b[3])*/
+/*				  +Q[1*3+2]*(Ainv[4*3+n]*(1.0+b[2])+Ainv[4*2+n]*b[3]));*/
 
-		F[2+n*3]+= alpha*(Q[0*3+0]*Ainv[4*1+n]*c[1]
-				  +Q[1*3+1]*Ainv[4*2+n]*c[2]
-				  +Q[2*3+2]*Ainv[4*3+n]*(1.0+c[3])
-				  +Q[0*3+1]*(Ainv[4*2+n]*c[1]+Ainv[4*1+n]*c[2])
-				  +Q[0*3+2]*(Ainv[4*1+n]*(1.0+c[3])+Ainv[4*3+n]*c[1])
-				  +Q[1*3+2]*(Ainv[4*2+n]*(1.0+c[3])+Ainv[4*3+n]*c[2]));
-	}//n
+/*		F[2+n*3]+= alpha*(Q[0*3+0]*Ainv[4*1+n]*c[1]*/
+/*				  +Q[1*3+1]*Ainv[4*2+n]*c[2]*/
+/*				  +Q[2*3+2]*Ainv[4*3+n]*(1.0+c[3])*/
+/*				  +Q[0*3+1]*(Ainv[4*2+n]*c[1]+Ainv[4*1+n]*c[2])*/
+/*				  +Q[0*3+2]*(Ainv[4*1+n]*(1.0+c[3])+Ainv[4*3+n]*c[1])*/
+/*				  +Q[1*3+2]*(Ainv[4*2+n]*(1.0+c[3])+Ainv[4*3+n]*c[2]));*/
+/*	}//n*/
 
 
 	//add user calculated force (UserDefined.h)
--- makefile
+++ makefile
@@ -1,13 +1,31 @@
+
+OBJECTS = clparse.o genrand.o physics_model.o output_writer.o parameters_reader.o parameters_writer.o main.o 
+EXTLIB = extlib/gmsh_io/libgmsh_io.a extlib/jsmn/libjsmn.a
+FLAGS = -lcurand -std=c++11 -Wno-deprecated-gpu-targets
+
+CPP_FILES := $(wildcard src/*.cpp)
+OBJ_FILES := $(addprefix obj/,$(notdir $(CPP_FILES:.cpp=.o)))
+
 ext:
 	cd extlib/jsmn && $(MAKE) && $(MAKE) test
 	cd extlib/gmsh_io && $(MAKE) all
 
-all: ext main.cu clparse.cpp genrand.cpp parameters_reader.cpp parameters_writer.cpp
-	./build
+#all: ext main.cu clparse.cpp genrand.cpp physics_model.cpp parameters_reader.cpp parameters_writer.cpp
+#	./build
 	
+#build: $(OBJECTS)
+build: ext $(OBJ_FILES)
+	nvcc $(FLAGS) $(OBJ_FILES) $(EXTLIB) -o builds/gafe6
+	
+obj/%.o: src/%.cpp
+	nvcc -x cu $(FLAGS) -I. -dc $< -o $@
+	
+		
 clean:
+	rm -f *.o || true
 	cd VTKOUT && rm *.vtk || true && rm *.xyzv || true && rm *.dat || true
 	cd builds && rm * || true
+	rm obj/*.o || true
 	cd extlib/jsmn && $(MAKE) clean
 	cd extlib/gmsh_io && $(MAKE) clean
 	
